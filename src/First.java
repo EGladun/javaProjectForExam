5100b3978754a165ad371200aa0974dd891daace
Greeds <monstromag@mail.ru>
2020-01-16 20:04:45 +0300

    First commit

--- src/First.java
+++ src/First.java
@@ -1,11 +1,19 @@
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
 import java.io.IOException;
 
-public class First extends javax.servlet.http.HttpServlet {
-    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
 
-    }
+@WebServlet(urlPatterns = "/first", name="First") public class Firs extends HttpServlet {
+    public class First extends javax.servlet.http.HttpServlet {
+        protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response)
+                throws javax.servlet.ServletException, IOException {
 
-    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+        }
 
+        protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response)
+                throws javax.servlet.ServletException, IOException {
+
+
+        }
     }
 }
