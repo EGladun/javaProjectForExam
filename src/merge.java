113ee53b585de5e9a1c8db7e9bc3cbe20fc18c89
Greeds <monstromag@mail.ru>
2020-01-19 15:24:13 +0300

    debug merge class

--- src/merge.java
+++ src/merge.java
@@ -28,28 +28,30 @@ class merge {
             doc2 = db.parse(new File("C:\\Users\\monst\\OneDrive\\Рабочий стол\\Новая папка\\catalog (4).xml"));
             NodeList ndListFirstFile = doc.getElementsByTagName("CD");
             NodeList ndListDonwloadFile = doc2.getElementsByTagName("CD");
-
-
-            for (int i=0;i<ndListFirstFile.getLength();i++){
-                NodeList f= doc.getElementsByTagName("TITLE");
+            NodeList d=doc2.getElementsByTagName("TITLE");
+            NodeList f= doc.getElementsByTagName("TITLE");
+            for (int i=0;i<ndListFirstFile.getLength();){
                 String full =f.item(i).getTextContent().trim();
-                for(int n = 0; n<ndListDonwloadFile.getLength(); n++){
-                    NodeList d=doc2.getElementsByTagName("TITLE");
-                    String dell =d.item(i).getTextContent().trim();
+                for(int n = 0; n<ndListDonwloadFile.getLength();){
+                    String dell =d.item(n).getTextContent().trim();
                     if(full.equals(dell)){
                         Element deleted = (Element) doc.getElementsByTagName("CD").item(i);
                         deleted.getParentNode().removeChild(deleted);
                         System.out.println("yep");
+                        n++;
+                        i--;
                     }
                     else{
                         System.out.println(full);
+                        n++;
                     }
-                }
 
+                }
+                i++;
             }
-            for(int i=0;i<ndListDonwloadFile.getLength();i++){
-                Node nodeCD = doc.importNode(doc2.getElementsByTagName("CD").item(i), true);
-                ndListFirstFile.item(i).appendChild(nodeCD);
+            for(int v=0;v<ndListDonwloadFile.getLength();v++){
+                Node nodeCD = doc.importNode(doc2.getElementsByTagName("CD").item(v), true);
+                ndListFirstFile.item(v).appendChild(nodeCD);
             }
 
             TransformerFactory tFactory = TransformerFactory.newInstance();
